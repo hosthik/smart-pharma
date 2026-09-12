f30c0a7294af9d1230793e1027e0ad6f80a62382
unknown <hosthassen2024@gmail.com>
2026-09-12 11:15:41 +0300

    Update SmartPharma

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -13,6 +13,7 @@ async function bootstrap() {
 
   app.enableCors({
     origin: [
+      "https://smart-pharma-org.vercel.app",
       "http://localhost:3000",
       "http://localhost:3001",
       "http://127.0.0.1:3000",
